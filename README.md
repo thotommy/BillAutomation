abda89f8c155c8145c1a89544471f892da619c73
thotommy <thotommy94@gmail.com>
2023-01-10 01:20:17 -0500

    Fix the docker images and added to the read me to build the image and run the container locally.

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ This is a project which will be used to help people who are not capable of payin
 In order to achieve this, the project will aggregate the prices of each bill from various sites using selenium. If all bills have values in them, it will then proceed to send a sms message via twilio to the user stating the price of the bill for each various sites, and to reply "Yes" or "No" to pay them.
 
 Once a user message is received, if they sent a yes, then the bills will be paid. If no it will not get paid and remind the user the next day.
+
+Build the docker image and run using:
+```
+docker build -t receivemsgserver .
+docker run -d -p 8000:5000 <IMAGEID>
+```
--- ReceiveMsgServer/ReceiveMsgServer.py
+++ ReceiveMsgServer/ReceiveMsgServer.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, request, Response
 from twilio.twiml.messaging_response import MessagingResponse
-from flask_ngrok import run_with_ngrok
+# from flask_ngrok import run_with_ngrok
 
 
 app = Flask(__name__)
-run_with_ngrok(app)
+# run_with_ngrok(app)
+
+
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
 
@@ -20,15 +22,18 @@ def sms_reply():
         __write_file(yes)
 
     if no in body.lower():
-        resp.message("The bill will not be paid. You will get a reminder the next day until paid.")
+        resp.message(
+            "The bill will not be paid. You will get a reminder the next day until paid.")
         __write_file(no)
 
     return Response(str(resp), mimetype="application/xml")
+
 
 @app.route("/", methods=['GET'])
 def main():
     print("hello world")
     return str("hello world")
+
 
 @app.route("/getResp", methods=['GET'])
 def get_resp():
@@ -38,10 +43,12 @@ def get_resp():
     print(resp)
     return Response(resp, mimetype="text/plain")
 
+
 def __write_file(resp):
     f = open("resp.txt", "w")
     f.write(resp)
     f.close()
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
